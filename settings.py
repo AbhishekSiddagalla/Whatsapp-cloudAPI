bc0407364d29630d19d90e6774fe771e37c836f9
Abhishek Siddagalla <abhishek.s@clientservertech.com>
2025-05-30 19:20:47 +0530

    adding settings file

---
+++ settings.py
@@ -0,0 +1,28 @@
+
+
+#App id
+app_id = "699360759646454"
+
+#CloudAPI version
+api_version = "v22.0"
+
+# WhatsApp Business account id
+whatsapp_business_account_id = "1740776970130492"
+
+#access token
+token = ""
+
+# Sender contact number
+sender_phone_number = "15556423110"
+
+#sender phone number id
+phone_number_id = "567853929754432"
+
+# Recipient contact number
+to_phone_number = "+919676122148"
+
+media_file_path = "C:\\Users\AbhishekSiddagalla\my_tasks\Whatsapp_cloudAPI\order.png"
+
+# fb app = app-id|app-secret
+fb_app_access_token = "699360759646454|a10e33a4600cddc79e0d9226ae65d2ec"
+
